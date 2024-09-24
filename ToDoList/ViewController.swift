98a2705753952d3cd1b17886bc472492de4da623
Yuliya Filimonova <yuliya_nova@icloud.com>
2024-09-23 20:47:00 -0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  for ToDoList
+//
+//  Created by Iuliia Filimonova on 23.09.2024
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
